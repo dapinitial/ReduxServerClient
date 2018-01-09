dde454ea3f11054fe805bf5aa23d8569c21f247b
David Puerto <me@davidpuerto.com>
2018-01-09 13:29:52 -0800

    updating mongoose promise and adding a simple config

--- auth/server/index.js
+++ auth/server/index.js
@@ -1,24 +1,26 @@
 // Main starting point of the application
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const app = express();
-const router = require('./router');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const router = require("./router");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 // DB Setup
-mongoose.connect('mongodb://localhost:auth/auth');
+mongoose.connect("mongodb://localhost:auth/auth", {
+  useMongoClient: true
+});
 
 // App Setup
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 app.use(cors());
-app.use(bodyParser.json({ type: '*/*' }));
+app.use(bodyParser.json({ type: "*/*" }));
 router(app);
 
 // Server Setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
-console.log('Server listening on:', port);
+console.log("Server listening on:", port);
